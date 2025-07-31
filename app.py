6ee7304f5726a02a44739c16d9ce9ad7d66b856e
prakhar141 <prakhar.mathur2020@gmail.com>
2025-07-31 20:21:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -113,7 +113,9 @@ def ask_deepseek(context, query, history=[]):
 
     messages = [
         {"role": "system", "content": 
-         "You're Quiliffy, a witty and helpful BITSian senior. Answer using the given context only. Use emojis. Keep it engaging and informal."}
+         "You're BitsBuddy, a witty and helpful BITSian senior. Answer using the given context only. Use emojis. Keep it engaging and informal.but strict when needed. 
+
+If someone broke a rule or violated college policy, respond clearly that rules are important and explain them politely. Don't encourage excuses. Avoid giving second chances unless the document clearly allows it. Be empathetic, but firm. If rules allow reconsideration, mention the process to request it."}
     ]
 
     for h in history[-3:]:
