f067de3123220bf9ed1d064aba1d0e51af458aa2
prakhar141 <prakhar.mathur2020@gmail.com>
2025-08-04 20:03:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -182,7 +182,7 @@ with st.sidebar:
 st.markdown("""
 <hr style="margin-top: 40px;">
 <div style='text-align: center; color: #888; font-size: 14px;'>
-    Built with 🧠 by <b>Prakhar Mathur</b> · BITS Pilani · 
+    Built with ❤️ by <b>Prakhar Mathur</b> · BITS Pilani · 
     <br>📬 Email: <a href="mailto:f20240347@pilani.bits-pilani.ac.in">Contact Prakhar</a>
 </div>
 """, unsafe_allow_html=True)
