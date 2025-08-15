addcf05d0477d0e8c9d2f24758bf3bf1c1c5cb7e
prakhar141 <prakhar.mathur2020@gmail.com>
2025-08-15 15:33:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ st.set_page_config(page_title="BITS Buddy", layout="wide")
 col1, col2 = st.columns([1, 8])  # Adjust ratio as needed
 
 with col1:
-    st.image("bits_logo.png", width=50)
+    st.image("logo.png", width=50)
 
 with col2:
     st.markdown("<h1 style='margin-top: 0;'>BITS Buddy</h1>", unsafe_allow_html=True)
