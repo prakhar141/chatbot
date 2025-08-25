2f1d546d8b869a3f2b7b273f921941699afc900c
prakhar141 <prakhar.mathur2020@gmail.com>
2025-08-25 13:18:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 # cleaned_buddy.py
+import spacy
+from spacy.cli import download
+
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
+
 import os
 import time
 import hashlib
